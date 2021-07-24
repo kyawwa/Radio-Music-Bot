4028e43972badc1327d08967cb146baf2b463506
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-24 06:53:39 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -153,14 +153,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/LushaiMusic/VCMusicPlayer'),
+                InlineKeyboardButton("🔥 OWNER 🔥", url='https://t.me/DKBOTZHELP'),
             ],
             [
-               InlineKeyboardButton('👥 Group', url='https://t.me/iZaute/5'),
-               InlineKeyboardButton('Channel 📢', url='https://t.me/iZaute/6'),
+               InlineKeyboardButton('👥 Group', url='https://t.me/DK_BOTZ'),
+               InlineKeyboardButton('Channel 📢', url='https://t.me/DKBOTZ'),
             ],
             [
-               InlineKeyboardButton('🔰 How to Deploy 🔰', url='https://t.me/c/1481808444/131'),
+               InlineKeyboardButton('🔰 OWNER 🔰', url='https://t.me/DKBOTZHELP'),
         
             ]
         ]
